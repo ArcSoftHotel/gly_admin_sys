0094663f931e4f6f2a4146ba8c5cfcc5df9cb32f
Suyvolen <49968768+Suyvolen@users.noreply.github.com>
2020-12-29 07:21:25 +0800

    更新的sql文件

--- ArcSoftHotel-Servlet/ArcSoftHotel.sql
+++ ArcSoftHotel-Servlet/ArcSoftHotel.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80020
  File Encoding         : 65001
 
- Date: 27/12/2020 14:20:17
+ Date: 29/12/2020 07:20:01
 */
 
 SET NAMES utf8mb4;
@@ -33,6 +33,50 @@ CREATE TABLE `admin`  (
 INSERT INTO `admin` VALUES ('admin', 'admin');
 
 -- ----------------------------
+-- Table structure for book
+-- ----------------------------
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book`  (
+  `userid` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '房客的用户名',
+  `roomid` int(0) NOT NULL COMMENT '入住的房间号',
+  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '房客的真实姓名',
+  `document_type` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '证件类型',
+  `document_number` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '证件号码',
+  `face` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '人脸数据',
+  `checkin_time` datetime(0) NULL DEFAULT NULL COMMENT '入住时间',
+  `checkout_time` datetime(0) NULL DEFAULT NULL COMMENT '退房时间',
+  `time` datetime(0) NULL DEFAULT NULL COMMENT '时间',
+  `status` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '房客状态',
+  `phone_number` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
+  `adult` int(0) NULL DEFAULT NULL COMMENT '成人数量',
+  `children` int(0) NULL DEFAULT NULL COMMENT '儿童数量',
+  PRIMARY KEY (`userid`, `roomid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of book
+-- ----------------------------
+INSERT INTO `book` VALUES ('2020', 102, '亚兴酱', '身份证', '证件号', NULL, '2020-12-19 10:57:49', '2020-12-28 10:57:54', '2020-12-28 10:57:58', '退房', '13536777888', 1, 0);
+
+-- ----------------------------
+-- Table structure for consume
+-- ----------------------------
+DROP TABLE IF EXISTS `consume`;
+CREATE TABLE `consume`  (
+  `userid` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `roomid` int(0) NOT NULL COMMENT '房间号',
+  `roomprice` double(10, 2) NOT NULL COMMENT '房间价格',
+  `service_price` double(10, 2) NULL DEFAULT NULL COMMENT '服务消费',
+  `date` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '每一次消费时，根据记录的时间',
+  PRIMARY KEY (`userid`, `roomid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of consume
+-- ----------------------------
+INSERT INTO `consume` VALUES ('2020', 101, 100.00, 200.00, '2020-12-28 22:17:26');
+
+-- ----------------------------
 -- Table structure for room
 -- ----------------------------
 DROP TABLE IF EXISTS `room`;
@@ -48,8 +92,9 @@ CREATE TABLE `room`  (
 -- ----------------------------
 -- Records of room
 -- ----------------------------
-INSERT INTO `room` VALUES (101, 'standard', 100, '空闲', 'addr');
-INSERT INTO `room` VALUES (102, 'standard', 100, '空闲', 'addr');
+INSERT INTO `room` VALUES (101, '标准', 100, '空闲', 'addr');
+INSERT INTO `room` VALUES (102, '单人豪华', 200, '已预订', 'addr');
+INSERT INTO `room` VALUES (107, '单人标准', 100, '空闲', 'addr');
 INSERT INTO `room` VALUES (205, 'president', 200, '已入住', 'addr');
 INSERT INTO `room` VALUES (207, '双人标准', 200, '已入住', 'addr');
 
@@ -70,8 +115,29 @@ CREATE TABLE `staff`  (
 -- Records of staff
 -- ----------------------------
 INSERT INTO `staff` VALUES (1, '张三', '送餐', '工作中', '13555555556');
-INSERT INTO `staff` VALUES (2, '亚兴酱', '保洁', '忙呢', '13536777888');
+INSERT INTO `staff` VALUES (2, '亚兴酱', '保洁', '忙', '13536777888');
 INSERT INTO `staff` VALUES (3, '果果', '安保', '忙', '13666666666');
 INSERT INTO `staff` VALUES (4, '昊天', '程序', '秃头', '15666666666');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `userid` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名，唯一标识',
+  `password` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户密码',
+  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户真实姓名',
+  `phone_number` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
+  `vip` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户会员信息',
+  `date` datetime(0) NULL DEFAULT NULL COMMENT '入住时间',
+  `face` varbinary(255) NULL DEFAULT NULL COMMENT '人脸数据',
+  `status` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户状态，是否入住',
+  PRIMARY KEY (`userid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('2020', '123', '亚兴酱', '13536777888', '并不', '2020-12-28 10:49:52', NULL, '未入住');
 
 SET FOREIGN_KEY_CHECKS = 1;
