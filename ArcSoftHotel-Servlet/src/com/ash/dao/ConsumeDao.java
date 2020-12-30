e09d8b76b0e09be1cf0f26d7e139308633c97509
Suyvolen <49968768+Suyvolen@users.noreply.github.com>
2020-12-30 17:43:32 +0800

    消费记录的数据库操作类

--- ArcSoftHotel-Servlet/src/com/ash/dao/ConsumeDao.java
+++ ArcSoftHotel-Servlet/src/com/ash/dao/ConsumeDao.java
@@ -10,54 +10,77 @@ import java.util.ArrayList;
 import com.ash.entity.Consume;
 import com.ash.util.MySqlUtil;
 
-public class ConsumeDao extends MySqlUtil{
+public class ConsumeDao extends MySqlUtil {
 	private Connection conn = null;
-    private PreparedStatement psmt = null;
-    private ResultSet rs = null;
-    
-    public ArrayList<Consume> selectConsume(String userid) throws SQLException {
-    	ArrayList<Consume> array = new ArrayList<>();
-    	String sql = "select * from consume where userid=?";
-    	try {
-            conn=dataSource.getConnection();
-            psmt=conn.prepareStatement(sql);
-            psmt.setString(1,userid);
-            rs=psmt.executeQuery();
-            while(rs.next()){
-               Consume cons = new Consume();
-               cons.setUserid(rs.getString("userid"));
-               cons.setRoomid(rs.getInt("roomid"));
-               cons.setRoomprice(rs.getDouble("roomprice"));
-               cons.setService_price(rs.getDouble("service_price"));
-               cons.setDate(rs.getDate("date"));
-               array.add(cons);
-            }
-        } catch (SQLException throwables) {
-            conn.close();
-       	 throwables.printStackTrace();          
-        }
-   	 conn.close();
-   	 return array;
-    }
-    
-    public boolean updateConsume(Consume consume) throws SQLException {
-    	String sql = "update consume set userid=?,roomid=?,roomprice=?,service_price=?"
-    			+ " where date=?";
-    	try {
-    		conn=dataSource.getConnection();
-            psmt=conn.prepareStatement(sql);
-            psmt.setString(1,consume.getUserid());
-            psmt.setInt(2,consume.getRoomid());
-            psmt.setDouble(3,consume.getRoomprice());
-            psmt.setDouble(4,consume.getService_price());
-            psmt.setDate(5,consume.getDate());
-            int result=psmt.executeUpdate();
-            if(result<=0) return false;
-    	}catch (SQLException throwables) {
-     	   conn.close();
-           throwables.printStackTrace();
-       }
-    	conn.close();
-    	return true;	
-    }
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
+
+	public ArrayList<Consume> selectConsume(String userid) throws SQLException {
+		ArrayList<Consume> array = new ArrayList<>();
+		String sql = "select * from consume where userid=?";
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, userid);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				Consume cons = new Consume();
+				cons.setUserid(rs.getString("userid"));
+				cons.setRoomid(rs.getInt("roomid"));
+				cons.setRoomprice(rs.getDouble("roomprice"));
+				cons.setService_price(rs.getDouble("service_price"));
+				cons.setDate(rs.getDate("date"));
+				array.add(cons);
+			}
+		} catch (SQLException throwables) {
+			conn.close();
+			throwables.printStackTrace();
+		}
+		conn.close();
+		return array;
+	}
+
+	public boolean updateConsume(Consume consume) throws SQLException {
+		String sql = "update consume set roomid=?,roomprice=?,service_price=? where userid=?";
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);		
+			psmt.setInt(1, consume.getRoomid());
+			System.out.println(consume.getRoomid());
+			psmt.setDouble(2, consume.getRoomprice());
+			System.out.println(consume.getRoomprice());
+			psmt.setDouble(3, consume.getService_price());
+			System.out.println(consume.getService_price());
+			psmt.setString(4, consume.getUserid());
+			System.out.println(consume.getUserid());
+			int result = psmt.executeUpdate();
+			if (result <= 0) {
+				System.out.println(result);
+			    return false;
+			}	
+		} catch (SQLException throwables) {
+			conn.close();
+			throwables.printStackTrace();
+		}
+		conn.close();
+		return true;
+	}
+
+	public boolean deleteConsume(String userid, Integer roomid) throws SQLException {
+		String sql = "delete from consume where userid=? and roomid=?";
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, userid);
+			psmt.setInt(2, roomid);
+			int result = psmt.executeUpdate();
+			if (result <= 0)
+				return false;
+		} catch (SQLException throwables) {
+			conn.close();
+			throwables.printStackTrace();
+		}
+		conn.close();
+		return true;
+	}
 }
